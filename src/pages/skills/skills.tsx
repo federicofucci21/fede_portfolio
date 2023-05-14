2925625227bd61e2f25cb4fd4e5af4cc87913454
federicofucci21 <federicofucci@hotmail.com>
2023-05-14 13:06:22 -0300

    skills dark css

--- src/pages/skills/skills.tsx
+++ src/pages/skills/skills.tsx
@@ -1,15 +1,20 @@
 import style from "./skills.module.css"
 import { technologies } from "../../assets/technologies/technologies";
+import { useSelector } from 'react-redux'
+import { RootState } from "../../redux/store";
 
 export default function Skills() {
+
+  const theme = useSelector((state: RootState) => state.theme.theme);
+
   return (
-    <div className={style.skills} id="techstack">
+    <div className={theme === "light" ? style.skills : style.skillsD} id="techstack">
       <section className={style.section}>
         <div className={style.cards}>
           {technologies.map((e) => (
             <div className={style.divCard}>
-              <img className={style.imgCard} src={e.url} alt={e.name} />
-              <p className={style.pCard}>{e.name}</p>
+              <img className={theme === "light" ? style.imgCard : style.imgCardD} src={e.url} alt={e.name} />
+              <p className={theme === "light" ? style.pCard : style.pCardD}>{e.name}</p>
             </div>
           ))}
         </div>
